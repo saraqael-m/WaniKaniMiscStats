2f8b4f7a2a0f16683c4280421f9ecb1e94d796ce
saraqael-m <80643194+saraqael-m@users.noreply.github.com>
2022-04-19 00:16:37 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ async function userinfo() {
     var userInfo = "";
     userInfo += fixHtml("<b>Username: ") + s["data"]["username"] + "\n";
     userInfo += fixHtml("<b>Level: ") + s["data"]["level"] + "\n";
-    userInfo += fixHtml("<b>Subscritppion active: ") + s["data"]["subscription"]["active"] + "\n";
+    userInfo += fixHtml("<b>Subscription active: ") + s["data"]["subscription"]["active"] + "\n";
     var lbl = document.getElementsByClassName("userinfo")[0];
     lbl.innerHTML = userInfo;
 }
@@ -120,6 +120,12 @@ async function levelinfo() {
     var date;
     currentLevel = levelData[1];
     while (currentLevel != null) {
+        if (currentLevel["data"]["abandoned_at"] != null) {
+            j++;
+            prevDate = new Date(levelData[j]["data"]["created_at"]);
+            firstDate = new Date(prevDate.getTime());
+            chartDate = [];
+        }
         date = new Date(currentLevel["data"]["created_at"]);
         chartData.push([j, (date.getTime() - prevDate.getTime()) / (3600000 * 24)]);
         j++;
@@ -162,27 +168,68 @@ async function wordinfo() {
     // create array
     var j;
     var kd;
-    var bestWords  = [[-1, 0, 0, 0, 0], [-1, 0, 0, 0, 0], [-1, 0, 0, 0, 0], [-1, 0, 0, 0, 0], [-1, 0, 0, 0, 0]];
-    var worstWords = [[2, 0, 0, 0, 0], [2, 0, 0, 0, 0], [2, 0, 0, 0, 0], [2, 0, 0, 0, 0], [2, 0, 0, 0, 0]];
+    var bestWords = [[-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0]];
+    var worstWords = [[2, 0, 1, 0, 0], [2, 0, 1, 0, 0], [2, 0, 1, 0, 0], [2, 0, 1, 0, 0], [2, 0, 1, 0, 0]];
+    var bestWordsR = [[-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0]];
+    var worstWordsR = [[2, 0, 1, 0, 0], [2, 0, 1, 0, 0], [2, 0, 1, 0, 0], [2, 0, 1, 0, 0], [2, 0, 1, 0, 0]];
+    var bestWordsK = [[-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0]];
+    var worstWordsK = [[2, 0, 1, 0, 0], [2, 0, 1, 0, 0], [2, 0, 1, 0, 0], [2, 0, 1, 0, 0], [2, 0, 1, 0, 0]];
+    var bestWordsV = [[-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0], [-1, 0, 1, 0, 0]];
+    var worstWordsV = [[2, 0, 1, 0, 0], [2, 0, 1, 0, 0], [2, 0, 1, 0, 0], [2, 0, 1, 0, 0], [2, 0, 1, 0, 0]];
     var c = 0;
+    var type;
+    var id;
+    var cor;
+    var inc;
+    var used = [];
     for (let i = 0; i < loops; i++) {
         j = 0;
         currentData = wordData[0];
         while (currentData != null) {
             c++;
             currentData = currentData["data"];
+            id = currentData["subject_id"];
+            if (used.findIndex(element => (element == id)) != -1) {
+                j++;
+                currentData = wordData[j];
+                if (c >= totPages) { break; }
+                continue;
+            } else {
+                used.push(id);
+            }
             kd = (currentData["meaning_correct"] / (currentData["meaning_incorrect"] + currentData["meaning_correct"]) + currentData["reading_correct"] / (currentData["reading_incorrect"] + currentData["reading_correct"])) / 2
             kdWeight = (currentData["meaning_correct"] / (currentData["meaning_incorrect"] + currentData["meaning_correct"] + 0.01) + currentData["reading_correct"] / (currentData["reading_incorrect"] + currentData["reading_correct"] + 0.01)) / 2
-            foundBest  =  bestWords.findIndex(element => (element[0] < kdWeight));
+            foundBest = bestWords.findIndex(element => (element[0] < kdWeight));
             foundWorst = worstWords.findIndex(element => (element[0] > kdWeight));
-            if (currentData["subject_id"] == 8952) {
-                console.log("herere!!!");
+            foundBestR = bestWordsR.findIndex(element => (element[0] < kdWeight));
+            foundWorstR = worstWordsR.findIndex(element => (element[0] > kdWeight));
+            foundBestK = bestWordsK.findIndex(element => (element[0] < kdWeight));
+            foundWorstK = worstWordsK.findIndex(element => (element[0] > kdWeight));
+            foundBestV = bestWordsV.findIndex(element => (element[0] < kdWeight));
+            foundWorstV = worstWordsV.findIndex(element => (element[0] > kdWeight));
+            type = currentData["subject_type"]
+            cor = currentData["meaning_correct"] + currentData["reading_correct"];
+            inc = currentData["meaning_incorrect"] + currentData["reading_incorrect"];
+            if (foundBest != -1) {
+                bestWords[foundBest] = [kdWeight, kd, id, cor, inc];
             }
-            if (foundBest != -1 && bestWords.findIndex(element => (element[2] == currentData["subject_id"])) == -1) {
-                bestWords[foundBest] = [kdWeight, kd, currentData["subject_id"], currentData["meaning_correct"] + currentData["reading_correct"], currentData["meaning_incorrect"] + currentData["reading_incorrect"]];
+            if (type == "radical" && foundBestR != -1) {
+                bestWordsR[foundBestR] = [kdWeight, kd, id, cor, inc];
+            } else if (type == "kanji" && foundBestK != -1) {
+                bestWordsK[foundBestK] = [kdWeight, kd, id, cor, inc];
+            } else if (type == "vocabulary" && foundBestV != -1) {
+                bestWordsV[foundBestV] = [kdWeight, kd, id, cor, inc];
             }
-            if (foundWorst != -1 && worstWords.findIndex(element => (element[2] == currentData["subject_id"])) == -1) {
-                worstWords[foundWorst] = [kdWeight, kd, currentData["subject_id"], currentData["meaning_correct"] + currentData["reading_correct"], currentData["meaning_incorrect"] + currentData["reading_incorrect"]];
+            if (foundWorst != -1) {
+                worstWords[foundWorst] = [kdWeight, kd, id, cor, inc];
+            }
+            if (type == "radical" && foundWorstR != -1) {
+                console.log(currentData);
+                worstWordsR[foundWorstR] = [kdWeight, kd, id, cor, inc];
+            } else if (type == "kanji" && foundWorstK != -1) {
+                worstWordsK[foundWorstK] = [kdWeight, kd, id, cor, inc];
+            } else if (type == "vocabulary" && foundWorstV != -1) {
+                worstWordsV[foundWorstV] = [kdWeight, kd, id, cor, inc];
             }
             j++;
             currentData = wordData[j];
@@ -202,9 +249,11 @@ async function wordinfo() {
         data = await promise;
         reviewData = data["data"];
     }
-    console.log(c, c, c, c, c, c, c);
+    console.log(bestWordsK);
+    console.log(bestWordsK);
+    console.log(bestWordsK);
 
-    // create numbering
+    // create numbering all
     wordsLbl = "";
     for (let i = 0; i < bestWords.length; i++) {
         let promise = fetch(new Request('https://api.wanikani.com/v2/subjects/' + bestWords[i][2], { method: 'GET', headers: requestHeaders }))
@@ -216,7 +265,7 @@ async function wordinfo() {
     var lbl = document.getElementById("topwords");
     lbl.innerHTML = wordsLbl;
     console.log(worstWords);
-    // create numbering
+    // create numbering all
     wordsLbl = "";
     for (let i = 0; i < worstWords.length; i++) {
         let promise = fetch(new Request('https://api.wanikani.com/v2/subjects/' + worstWords[i][2], { method: 'GET', headers: requestHeaders }))
@@ -226,6 +275,75 @@ async function wordinfo() {
         wordsLbl += word["data"]["characters"] + " | correct: " + worstWords[i][3] + " | incorrect: " + worstWords[i][4] + " | percentage: " + parseInt(worstWords[i][1] * 100) + "%\n"
     }
     var lbl = document.getElementById("worstwords");
+    lbl.innerHTML = wordsLbl;
+    // create numbering rad
+    wordsLbl = "";
+    for (let i = 0; i < bestWordsR.length; i++) {
+        let promise = fetch(new Request('https://api.wanikani.com/v2/subjects/' + bestWordsR[i][2], { method: 'GET', headers: requestHeaders }))
+            .then(response => response.json())
+            .then(responseBody => responseBody);
+        let word = await promise;
+        wordsLbl += word["data"]["characters"] + " | correct: " + bestWordsR[i][3] + " | incorrect: " + bestWordsR[i][4] + " | percentage: " + parseInt(bestWordsR[i][1] * 100) + "%\n"
+    }
+    var lbl = document.getElementById("topwordsradical");
+    lbl.innerHTML = wordsLbl;
+    console.log(worstWords);
+    // create numbering rad
+    wordsLbl = "";
+    for (let i = 0; i < worstWordsR.length; i++) {
+        let promise = fetch(new Request('https://api.wanikani.com/v2/subjects/' + worstWordsR[i][2], { method: 'GET', headers: requestHeaders }))
+            .then(response => response.json())
+            .then(responseBody => responseBody);
+        let word = await promise;
+        wordsLbl += word["data"]["characters"] + " | correct: " + worstWordsR[i][3] + " | incorrect: " + worstWordsR[i][4] + " | percentage: " + parseInt(worstWordsR[i][1] * 100) + "%\n"
+    }
+    var lbl = document.getElementById("worstwordsradical");
+    lbl.innerHTML = wordsLbl;
+    // create numbering kan
+    wordsLbl = "";
+    for (let i = 0; i < bestWordsK.length; i++) {
+        let promise = fetch(new Request('https://api.wanikani.com/v2/subjects/' + bestWordsK[i][2], { method: 'GET', headers: requestHeaders }))
+            .then(response => response.json())
+            .then(responseBody => responseBody);
+        let word = await promise;
+        wordsLbl += word["data"]["characters"] + " | correct: " + bestWordsK[i][3] + " | incorrect: " + bestWordsK[i][4] + " | percentage: " + parseInt(bestWordsK[i][1] * 100) + "%\n"
+    }
+    var lbl = document.getElementById("topwordskanji");
+    lbl.innerHTML = wordsLbl;
+    console.log(worstWords);
+    // create numbering kan
+    wordsLbl = "";
+    for (let i = 0; i < worstWordsK.length; i++) {
+        let promise = fetch(new Request('https://api.wanikani.com/v2/subjects/' + worstWordsK[i][2], { method: 'GET', headers: requestHeaders }))
+            .then(response => response.json())
+            .then(responseBody => responseBody);
+        let word = await promise;
+        wordsLbl += word["data"]["characters"] + " | correct: " + worstWordsK[i][3] + " | incorrect: " + worstWordsK[i][4] + " | percentage: " + parseInt(worstWordsK[i][1] * 100) + "%\n"
+    }
+    var lbl = document.getElementById("worstwordskanji");
+    lbl.innerHTML = wordsLbl;
+    // create numbering voc
+    wordsLbl = "";
+    for (let i = 0; i < bestWordsV.length; i++) {
+        let promise = fetch(new Request('https://api.wanikani.com/v2/subjects/' + bestWordsV[i][2], { method: 'GET', headers: requestHeaders }))
+            .then(response => response.json())
+            .then(responseBody => responseBody);
+        let word = await promise;
+        wordsLbl += word["data"]["characters"] + " | correct: " + bestWordsV[i][3] + " | incorrect: " + bestWordsV[i][4] + " | percentage: " + parseInt(bestWordsV[i][1] * 100) + "%\n"
+    }
+    var lbl = document.getElementById("topwordsvocab");
+    lbl.innerHTML = wordsLbl;
+    console.log(worstWords);
+    // create numbering voc
+    wordsLbl = "";
+    for (let i = 0; i < worstWordsV.length; i++) {
+        let promise = fetch(new Request('https://api.wanikani.com/v2/subjects/' + worstWordsV[i][2], { method: 'GET', headers: requestHeaders }))
+            .then(response => response.json())
+            .then(responseBody => responseBody);
+        let word = await promise;
+        wordsLbl += word["data"]["characters"] + " | correct: " + worstWordsV[i][3] + " | incorrect: " + worstWordsV[i][4] + " | percentage: " + parseInt(worstWordsV[i][1] * 100) + "%\n"
+    }
+    var lbl = document.getElementById("worstwordsvocab");
     lbl.innerHTML = wordsLbl;
 }
 
